0d10dfa5b479dbca29474d61d99e6956209e9300
Kelly Do <kellydo8467@gmail.com>
2024-05-01 13:35:36 -0400

    fixed gradient and image spacing

--- frontend/static/fetch.js
+++ frontend/static/fetch.js
@@ -14,29 +14,6 @@ const getToken = async () => {
 
     const data = await result.json();
     return data.access_token;
-    // console.log(data.access_token);
-}
-
-
-
-const getSongs = async (token, id) => {
-    const result = await fetch(`https://api.spotify.com/v1/playlists/${id}/tracks`, {
-        method: 'GET',
-        headers: {'Authorization': 'Bearer ' + token}
-    });
-
-    const items = await result.json();
-    const container = document.getElementsByClassName('container')[0];
-    container.innerHTML = ''; // Clear container's contents
-
-    items.items.forEach(item => {
-        const songName = item.track.name;
-        const pElement = document.createElement('p');
-        pElement.textContent = songName; // Set song name as text content
-        console.log(songName);
-        container.appendChild(pElement); // Append <p> element to song container
-    });
-
 }
 
 const displaySongs= async(token, id) => {
@@ -50,7 +27,6 @@ const displaySongs= async(token, id) => {
         const songName = item.track.name;
         const imgUrl = item.track.album.images[2].url
         console.log(imgUrl);
-        // const imgUrl = imgUrlList[0].url;
         const div = document.createElement('div');
         div.classList.add('track');
 
@@ -68,9 +44,19 @@ function nameAndImg (song, url, div){
     const p = document.createElement('p');
     p.textContent = song;
     div.appendChild(p)
-    // const img = document.createElement('img');
 }
-// getToken();
 
-getToken()
-  .then(data => displaySongs(data, "2qjr8xuEYrLoJLgDe6iHbd"));
+const getID = async () => {
+    const response = await fetch ("http://127.0.0.1:5000/home");
+    const id = response.statusText;
+    return id;
+}
+
+
+const run = async () => {
+    const token = await getToken();
+    const id = await getID();
+    displaySongs(token, id);
+}
+
+run()
